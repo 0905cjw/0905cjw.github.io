01e738cdb38732a1498abd354366b3651d235e58
3536c <3536c@CHOI>
2021-01-05 18:21:25 +0900

    ea

---
+++ Project/src/fd.java
@@ -0,0 +1,12 @@
+
+public class fd {
+
+	public fd() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String args[]) {
+		for(int i=19;i<29;i++) {
+			System.out.print("G"+i+"+");
+		}
+	}
+}
