881ce85511681a16687ffe657b2ff6b5e86dcbb5
0905cjw <0905cjw@gmail.com>
2021-06-05 00:18:52 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('#toggle').click(function(){
+		if($('.header_menu').hasClass('active')==true){
+			$('.header_menu').removeClass('active');
+		}else{
+			$('.header_menu').addClass('active');
+		}
+	});
+	
+	var height=window.innerHeight;
+	$('#home').css('height',height+'px');
+	$('#home_txt').css('line-height',height+'px');
+	$('#home>div').css('height', height+'px');
+});
