8f40b9d7c4ca4a05a1894bc427995b53b442cd69
0905cjw <0905cjw@gmail.com>
2021-06-05 00:31:57 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ $(function(){
 		}
 	});
 	
+	$('#menu>li').click(function(){
+		$('.header_menu').removeClass('active');
+	});
+	
 	var height=window.innerHeight;
 	$('#home').css('height',height+'px');
 	$('#home_txt').css('line-height',height+'px');
